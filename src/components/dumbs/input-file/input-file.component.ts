5967f87227f3707a15f6275c82610f5d82de1faa
QLFJameS <jms.gomez12@gmail.com>
2024-09-14 17:07:34 +0200

    add input-file / no link with api-upload

--- src/components/dumbs/input-file/input-file.component.ts
+++ src/components/dumbs/input-file/input-file.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-file',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './input-file.component.css'
 })
 export class InputFileComponent {
-
+@Input() text =""
 }
